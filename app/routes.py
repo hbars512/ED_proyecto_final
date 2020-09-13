bfb8b4f07b2773571ca6845d46f33954c4fd0918
Herbert Arias <dayanqwe123@gmail.com>
2020-09-13 18:23:34 -0500

    feat: algoritmo grafo puente

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
 from flask import render_template
 from app import app
+from app.graph_bridge import GraphBridge
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Herbert'}
-    return render_template('index.html', title='Home', user=user)
+    grafo1 = GraphBridge(5)
+    grafo1.addEdge(1, 0)
+    grafo1.addEdge(0, 2)
+    grafo1.addEdge(2, 1)
+    grafo1.addEdge(0, 3)
+    grafo1.addEdge(3, 4)
+    grafo1.bridge()
+
+    return render_template('index.html', title='Proyecto ED', grafo=grafo1)
