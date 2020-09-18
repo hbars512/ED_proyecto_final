b351caa64dd24f301dbf19c4cf9a2a2c5b4b2b1e
Herbert Arias <dayanqwe123@gmail.com>
2020-09-17 20:04:26 -0500

    fea: bridges avances

--- app/routes.py
+++ app/routes.py
@@ -11,16 +11,18 @@ def index():
 
 @app.route('/bridges', methods=['GET', 'POST'])
 def bridges():
+    grafo1 = None
     if request.method == 'POST':
         nodos = int(request.form['num_nodos'])
-        bridges = request.form['num_nodos']
+        aristas = request.form['num_aristas']
 
-    grafo1 = GraphBridge(5)
-    grafo1.addEdge(1, 0)
-    grafo1.addEdge(0, 2)
-    grafo1.addEdge(2, 1)
-    grafo1.addEdge(0, 3)
-    grafo1.addEdge(3, 4)
-    grafo1.bridge()
+        grafo1 = GraphBridge(nodos)
+        for arista in aristas.split(', '):
+            nodos = []
+            for nodo in arista.split('-'):
+                nodos.append(int(nodo))
+            grafo1.addEdge(nodos[0], nodos[1])
+
+        grafo1.bridge()
 
     return render_template('bridge.html', title='Proyecto ED', grafo=grafo1)
