d5aec713bf50f53fcaafe277abcf6f6fce77599c
Herbert Arias <dayanqwe123@gmail.com>
2020-09-18 11:37:23 -0500

    feat: proyecto funcionando 100 de 100

---
+++ app/graph_cycles.py
@@ -0,0 +1,75 @@
+# Python Program to count
+# cycles of length n
+# in a given graph.
+
+
+class GraphCycles:
+
+    def __init__(self, vertices, graph):
+        self.V = vertices  # No. of vertices
+        self.graph = graph
+
+    def DFS(self, marked, n, vert, start, count):
+
+        # mark the vertex vert as visited
+        marked[vert] = True
+
+        # if the path of length (n-1) is found
+        if n == 0:
+            # mark vert as un-visited to make
+            # it usable again.
+            marked[vert] = False
+
+            # Check if vertex vert can end with
+            # vertex start
+            if self.graph[vert][start] == 1:
+                count = count + 1
+                return count
+            else:
+                return count
+
+        # For searching every possible path of
+        # length (n-1)
+        for i in range(self.V):
+            if marked[i] is False and self.graph[vert][i] == 1:
+
+                # DFS for searching path by decreasing
+                # length by 1
+                count = self.DFS(marked, n-1, i, start, count)
+
+        # marking vert as unvisited to make it
+        # usable again.
+        marked[vert] = False
+        return count
+
+    # Counts cycles of length
+    # N in an undirected
+    # and connected graph.
+    def count_cycles(self, n):
+
+        # all vertex are marked un-visited initially.
+        marked = [False] * self.V
+
+        # Searching for cycle by using v-n+1 vertices
+        count = 0
+        for i in range(self.V-(n-1)):
+            count = self.DFS(marked, n-1, i, i, count)
+
+            # ith vertex is marked as visited and
+            # will not be visited again.
+            marked[i] = True
+
+        return int(count/2)
+#
+# # main :
+# graph = [[0, 1, 0, 1, 0],
+#          [1, 0, 1, 0, 1],
+#          [0, 1, 0, 1, 0],
+#          [1, 0, 1, 0, 1],
+#          [0, 1, 0, 1, 0]]
+#
+# relol = CyclesCount(5)
+#
+# n = 4
+# print("Total cycles of length ", n, " are ", relol.countCycles(graph, n))
+# this code is contributed by Shivani Ghughtyal
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, url_for, request, redirect
 from app import app
 from app.graph_bridge import GraphBridge
+from app.graph_cycles import GraphCycles
+import numpy as np
 
 
 @app.route('/')
@@ -37,4 +39,35 @@ def bridges():
 
 @app.route('/cycles', methods=['GET', 'POST'])
 def cycles():
+    if request.method == 'POST':
+        n_nodos = int(request.form['num_nodos'])
+        aristas = request.form['aristas']
+        n_ciclos = int(request.form['n_ciclos'])
+
+        nodos = []
+        for arista in aristas.split(', '):
+            row_nodos = []
+            for nodo in arista.split('-'):
+                row_nodos.append(int(nodo))
+            nodos.append(row_nodos)
+
+        graph = np.zeros((n_nodos, n_nodos))
+        for i in range(n_nodos):
+            for j in range(n_nodos):
+                if [i, j] in nodos:
+                    graph[i][j] = 1
+                    graph[j][i] = 1
+
+        grafo1 = GraphCycles(n_nodos, graph)
+        num_ciclos = grafo1.count_cycles(n_ciclos)
+
+        return render_template(
+            'cycles.html',
+            title='Proyecto ED',
+            aristas=aristas,
+            n_ciclos=n_ciclos,
+            num_ciclos=num_ciclos,
+            numero_nodos=n_nodos
+        )
+
     return render_template('cycles.html')
